1d01d9bf6a98a19f486f83e4b5a3b1740d83782b
vinimorais2002 <vdemorais2002@outlook.com>
2022-11-17 21:01:39 -0300

    Teste_vscode

---
+++ estruturas_de_controle/src/teste/HelloWorld.java
@@ -0,0 +1,14 @@
+package teste;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("oi isso é um teste. :)");
+
+        double x = 2.0;
+        double y = 3.0;
+
+        double soma = x + y;
+
+        System.out.println(soma);
+    }
+}
